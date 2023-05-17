5fd31e1b1bc526ff2c74ea4de107e75e7725e40d
vadimvynnchuk <vadimvinnicuk4@gmail.com>
2023-05-17 18:25:30 +0300

    change

--- _dist/js/scripts.js
+++ _dist/js/scripts.js
@@ -263,6 +263,25 @@ $(function() {
 
 
 
+	//quantity-input
+	  
+	  $('.plus').click(function() {
+		var input = $('#quantity-input');
+		var value = parseInt(input.val());
+		input.val(value + 1);
+	  });
+	
+	  
+	  $('.minus').click(function() {
+		var input = $('#quantity-input');
+		var value = parseInt(input.val());
+		if (value > 1) {
+		  input.val(value - 1);
+		}
+	  });
+
+
+
 })
 
 
