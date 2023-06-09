288bdef19502928d9f4a91a7bbdda17c9e976204
vadimvynnchuk <vadimvinnicuk4@gmail.com>
2023-06-09 16:25:03 +0300

    change

--- _src/js/scripts.js
+++ _src/js/scripts.js
@@ -97,13 +97,13 @@ $(function() {
 			$(".btn-open-search").css('display','none'),
 			$(".close-open-search").css('display','block')
 			$("html, body").animate({ scrollTop: 0 }, "slow");
-			$('.fix-cart').css('display','none');
+			$('.fix-cart').css('right','-200px');
 
 		} else {
 			$(".btn-open-search").css('display','block')
 			$(".close-open-search").css('display','none')
 			$('main').removeClass('for-main-overflow');
-			$('.fix-cart').css('display','flex');
+			$('.fix-cart').css('right','20px');
 		}
 	})
 
